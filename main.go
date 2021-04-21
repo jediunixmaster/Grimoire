e1cf85c7760d2d3a3de09059202e886517d7319a
steve young <jediunixmaster@gmail.com>
2021-04-21 05:59:09 -0700

    added http header and fixed img link

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,6 +16,20 @@ func main() {
 			log.Println(err)
 		}
 		defer f.Close()
+		var contentType string
+		switch {
+		case strings.HasSuffix(r.URL.Path, "css"):
+			contentType = "text/css"
+		case strings.HasSuffix(r.URL.Path, "html"):
+			contentType = "text/html"
+		case strings.HasSuffix(r.URL.Path, "png"):
+			contentType = "text/png"
+		case strings.HasSuffix(r.URL.Path, "png"):
+			contentType = "text/svg"
+		default:
+			contentType = "text/plain"
+		}
+		w.Header().Add("Content-Type", contentType)
 		io.Copy(w, f)
 		//w.Write([]byte("Hello Steve It's working"))
 	})
